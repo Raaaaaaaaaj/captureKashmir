6542a71aa557e5e7c7ed590ad851dcc4de3dfde6
Indra <144indra7@gmail.com>
2025-02-06 22:33:51 +0530

    ck

--- js/musicprev.js
+++ js/musicprev.js
@@ -23,8 +23,7 @@
 // };
 document.addEventListener("DOMContentLoaded", function () {
     let music = document.getElementById("bgMusic");
-    setTimeout(()=>{document.querySelector('.rightbtn').click();}, 2000);
-    console.log('clicked');
+    // document.querySelector('.rightbtn').click();
     // Check if the audio element already exists
     if (!music) {
         // Create and configure audio element
